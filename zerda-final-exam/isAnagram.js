3a4fa69980f3cff9e2a08c238d2e8aefdf4789ce
Jessie Cai <tsai19930224@gmail.com>
2017-11-01 10:18:07 +0800

    add another function

--- zerda-final-exam/isAnagram.js
+++ zerda-final-exam/isAnagram.js
@@ -9,3 +9,32 @@ function isAnagram(str1, str2) {
     throw new Error('This function requires two string as parameter');
   }
 }
+
+function isAnagram2(str1, str2) {
+  if (str1 && str2 && typeof str1 === 'string' && typeof str2 === 'string') {
+    let str1Count = {};
+    for (let i = 0; i < str1.length; i++) {
+      const ch = str1[i];
+      if (ch !== ' ') {
+        str1Count[ch] = str1Count[ch] ? str1Count[ch] + 1 : 1; 
+      }
+    }
+    for (let j = 0; j < str2.length; j++) {
+      const ch = str2[j];
+      if (ch !== ' ') {
+        if(!str1Count[str2[i]]) {
+          return false;
+        } else {
+          str1Count[str2[i]] -= 1;
+        }
+      }
+    }
+    const str1CountKeys = Object.keys(str1Count);
+    for (let k = 0; k < str1CountKeys.length; k++) {
+      // will check str1Count's value
+      // make sure all the value in it is 0
+    }
+  } else {
+    throw new Error('This function requires two string as parameter');
+  }
+}
