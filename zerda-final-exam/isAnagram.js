2e8d5eb94f805bfc7b1fb5f1b9091cc2d5c57855
Jessie Cai <tsai19930224@gmail.com>
2017-11-01 10:02:04 +0800

    change parameter name

--- zerda-final-exam/isAnagram.js
+++ zerda-final-exam/isAnagram.js
@@ -2,9 +2,9 @@
 
 function isAnagram(str1, str2) {
   if (str1 && str2 && typeof str1 === 'string' && typeof str2 === 'string') {
-    const Str1 = str1.toLowerCase().split('').sort().join('').trim();
-    const Str2 = str2.toLowerCase().split('').sort().join('').trim();
-    return Str1 === Str2;
+    const word1 = str1.toLowerCase().split('').sort().join('').trim();
+    const word2 = str2.toLowerCase().split('').sort().join('').trim();
+    return word1 === word2;
   } else {
     throw new Error('This function requires two string as parameter');
   }
